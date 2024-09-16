10b3017a090a59d29b09f5af028f8651d055825b
kmaitreys <km.maitrey@gmail.com>
2024-09-16 18:34:00 +0530

    add Dust header doc

---
+++ src/Dust.jl
@@ -0,0 +1,9 @@
+"""
+=======================================================================
+                         DUST OPACITY MODULE
+
+ This dust opacity module contains pieces of code for Mie scattering that
+ the author got from Alex de Koter (University of Amsterdam) who presumably
+ (?)  got the Mie routine from Joop Hovenier (?).
+=======================================================================
+"""
