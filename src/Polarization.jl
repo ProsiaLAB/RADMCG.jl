d15831d71dacedf740ce0e72ace2da7cbc32a1ca
kmaitreys <km.maitrey@gmail.com>
2024-09-23 21:07:00 +0530

    add Polarization header doc

--- src/Polarization.jl
+++ src/Polarization.jl
@@ -155,6 +155,313 @@
 ! 2*ang.
 !
 !------------------------------------------------------------------------
+!
+! SCATTERING OFF RANDOMLY ORIENTED NON-SPHERICAL PARTICLES
+!
+! Now let's look at the scattering. For this we need the scattering Mueller
+! matrix Z(theta,phi). Here theta is the degree of directional change in
+! radian, with theta=0 meaning forward scattering, theta=pi/2 is
+! perpendicular scattering and theta=pi is back-scattering. The phi angle is
+! the scattering direction in the (x',y') plane. It defines the "scattering
+! plane" spanned by the incoming and outgoing photon direction. For phi=0
+! this scattering plane is in the (x',y')=(1,0) direction. In that case,
+! the S-vector acts as the rotation axis of the scattering. 
+!
+! NOTE: Our convention is that the local 2-D (x',y') basis is such that it
+! defines a 2-D plane perpendicular to the INCOMING photon. This photon is
+! pointing toward the observer, i.e. if we make 3-D vectors of the x'- and
+! y'-basis: e_x' and e_y', then e_z'=cross(e_x',e_y') points toward the
+! observer and gives the direction of propagation of the incoming photon.
+! The S-vector is by definition e_y'. If phi=0 and 0<theta<pi, the
+! scattering takes place in the x'-direction, i.e. in 3-D this is the plane
+! spanned by (e_x',e_z'), i.e. the plane perpendicular to S==e_y'. For phi=0
+! and 0<theta<pi the new photon propagation direction after scattering will
+! be in positive x' direction, i.e. n_new = a_z*e_z' + a_x*e_x', with
+! a_x>=0.
+!
+! The Stokes parameters of the scattered flux off a single grain can be
+! written in terms of the Stokes parameters of the incoming flux using the
+! scattering matrix Z:
+! 
+!   ( I_scat )            ( Z_11   Z_12   Z_13   Z_14 ) ( I_inc )
+!   ( Q_scat )    m_grain ( Z_21   Z_22   Z_23   Z_24 ) ( Q_inc )
+!   ( U_scat )  = ------- ( Z_31   Z_32   Z_33   Z_34 ) ( U_inc ) 
+!   ( V_scat )      r^2   ( Z_41   Z_42   Z_43   Z_44 ) ( V_inc )
+!
+! where (I,Q,U,V)_inc = flux_inc and (I,Q,U,V)_scat = flux_scat. The
+! scattering matrix is Z is the matrix-valued differential cross section for
+! the scattering per unit mass of dust (hence the factor m_grain in the
+! equation above). Note that the book of van de Hulst calls this matrix F
+! instead of Z. The book by Mishchenko calls it Z for aligned particles, but
+! F for randomly oriented particles, because in these two cases they
+! define the (x',y')_inc and (x',y')_scat basis vectors differently
+! (in the fixed alignment case the particle orientation is fixed in
+! the lab frame while in the randomly oriented particle case the 
+! x',y' for incoming radiation is oriented w.r.t. the outgoing scattering
+! angle). For exact normalizations of Z, see below in the section
+! comparing our conventions to standard books.
+!
+! For randomly oriented particles we need to specify Z only as a function of
+! theta. This is, however, only true for randomly oriented particles for
+! which each helicity has equal amount of counter-helicity. If we have
+! magnetic or gravitational alignment of particles then the full
+! Z(theta,phi) must be specified.
+!
+! The scattering matrix Z(theta) is thus given for a '-basis such that the
+! S-vector is perpendicular to the scattering plane. Typically your photon
+! initially has some arbitrary perpendicular S-vector. Scattering may then
+! take place in any arbitrary scattering plane. If we want to use the
+! Z(theta), for that scattering direction, you first have to rotate the
+! '-basis to the ''-basis for which the NEW S-vector (i.e. the
+! x''-direction) is again perpendicular to the scattering plane. Then we can
+! apply the Z(theta), giving us the new (I,Q,U,V). This also automatically
+! defines the new orientation of the (x',y') plane which, after the
+! scattering event, is of course also rotated in 3-D space. The 3-D version
+! of the y' axis stays unaltered during scattering and the 3-D version of
+! the x'-axis is rotated around the y'-axis (=S-vector) by an angle theta
+! in right-handed direction (if you put the S-vector pointing toward you,
+! the x' axis rotates counter-clockwise).
+!
+! If we want to calculate the scattering source function in a given
+! direction of the observer, for a given incident angle of radiation, we
+! know precisely the theta and phi angles in advance. We can then indeed
+! simply rotate the '-basis, multiply the scattering matrix, and rotate to
+! the user-specified S-vector. This is relatively simple, and is done
+! by the subroutine polarization_randomorient_scatsource().
+! 
+! However, if we want to choose a new photon direction and polarization for
+! a photon package in a Monte Carlo code, then we do not know the theta and
+! phi in advance. In fact, we run into the problem that we cannot a-priori
+! rotate the '-basis into the scattering direction.  On the contrary, we
+! want to know the scattering probability as a function of theta and phi, so
+! we NEED the full Z(theta,phi) rather than just Z(theta). Fortunately we
+! can still do this with rotation, because multiplying with Z(theta,phi) is
+! the same as (1) first rotating the basis by angle ang=phi (using the
+! formulae above), (2) multiplying with the Z(theta) matrix, and (3)
+! rotating back to the original basis by choosing ang=-phi. 
+!
+! So let us assume that we have the full scattering matrix Z(theta,phi). Then
+! if we integrate the outgoing I_out(theta,phi) over 4*pi for a given
+! unpolarized input intensity I, we should get kappa_scat*I:
+!
+!      /+1             /2pi                             
+!      |   dcos(theta) |   dphi  Z(theta,phi)[1,1] * I   
+!      /-1             /0                                
+! 
+!                               = kappa_scat * I
+!
+! That is how we define the normalization of the scattering Mueller
+! matrix. The Z(theta,phi)[1,1] is thus the angular differential cross
+! section of scattering per unit mass of dust. For isotropic scattering it
+! would thus be Z11=kappa_scat/(4*pi).
+!
+! For polarized input intensity this should hold as well, as long as the
+! grains are randomly oriented and have no netto helicity (i.e. equal
+! left- and right-helicity). 
+!
+! We can verify this by doing the integrals for the q, u and v components:
+!
+!                      /+1     /2pi      4                      
+! kappa_scat(q,u,v) =  |   dmu |   dphi Sum Z(theta,phi)[1,i] * (1,q,u,v)[i]
+!                      /-1     /0       i=1                         
+!
+! where mu=cos(theta0), q=Q/I, u=U/I, v=V/I (i.e. all values between -1 and 1).
+! We can work out this integral a bit deeper, because the integration over
+! dphi can be done as follows:
+! 
+!                      /+1      4                  /2pi         
+! kappa_scat(q,u,v) =  |   dmu Sum Z(theta)[1,i] * |   dphi Rotate_phi{(1,q,u,v)}[i]
+!                      /-1     i=1                 /0               
+!
+! i.e. we now have only Z(mu) instead of Z(mu,phi). We have
+!
+!  Rotate_phi{(1,q,u,v)} = 
+!     (1,cos(2*phi)*q+sin(2*phi)*u,-sin(2*phi)*q+cos(2*phi)*u,v)
+!
+! The integral over 2*pi of the middle two components is 0, so we obtain
+!
+!                          /+1      4 
+! kappa_scat(q,u,v) = 2 pi |   dmu Sum Z(theta)[1,i] * (1,0,0,v)[i]
+!                          /-1     i=1
+!
+!                          /+1      
+!                   = 2 pi |   dmu ( Z[1,1] + Z[1,4]*v )
+!                          /-1     
+!
+! As one can see, linearly polarized light has the same scattering cross
+! section as unpolarized light. This is because of the random orientation
+! of the dust particles, and will NOT be the case when the particles are
+! somehow aligned. But note that circularly polarized light might have
+! a different scattering cross-section if Z[1,4].ne.0. This means that if
+! we have netto helicity of our particles, then we also have dependence
+! on the input polarization state. 
+!
+! For scattering off randomly oriented particles with a plane of symmetry
+! (i.e. non-helical) the scattering matrix takes the form (e.g. Mishchenko et
+! al. 1996 JQRST 55, 535):
+!
+!           ( Z_11   Z_12    0      0   )
+!           ( Z_12   Z_22    0      0   )
+!  Z(mu) =  (  0      0     Z_33   Z_34 )
+!           (  0      0    -Z_34   Z_44 )
+!
+! This has 6 independent matrix elements.
+!
+! For the somewhat more special case of Mie scattering off spherical
+! particles the scattering matrix is even somewhat simpler:
+!
+!           ( Z_11   Z_12    0      0   )
+!           ( Z_12   Z_11    0      0   )
+!  Z(mu) =  (  0      0     Z_33   Z_34 )
+!           (  0      0    -Z_34   Z_33 )
+!
+! This has only 4 independent matrix elements! A famous code that can
+! compute these, and is freely available on the internet, is the code
+! by Bohren & Huffman from the appendix of their book. A version that
+! has been improved by Bruce Draine in the early 90s is the bhmie code
+! from the scatterlib library:
+!
+!   http://code.google.com/p/scatterlib/
+!
+! The callbhmie() program from that bhmie library computes not only
+! the scattering and absorption cross sections, but also the Z_11,
+! Z_12, Z_33 and Z_34 (which they call S_11, S_12, S_33 and S_34,
+! but please see below for the different normalization). 
+!
+! In this code we allow for the 6-element matrix form, i.e. more general
+! than the Mie scattering case, i.e. allowing for prolate or oblate
+! particles. 
+!
+! In any case, for these randomly-oriented or spherical grains we have
+! Z_14=0, so that
+!
+!                           /+1      
+!  kappa_scat(q,u,v) = 2 pi |   dmu Z(mu)[1,1]
+!                           /-1     
+!
+! in other words:
+!
+!  kappa_scat(q,u,v) = kappa_scat
+!
+! This means that to find out the scattering cross section, we do not need
+! any information about the polarization state. This is good, because
+! otherwise even the unpolarized Monte Carlo scattering model would be
+! wrong, because the polarization state would then affect the further
+! scattering behavior. This is, in the case of randomly oriented and/or
+! spherical grains not the case. For now we shall use this as a working
+! hypothesis. NOTE: It is still not perfectly correct, because while the
+! total cross section for scattering is the same, the direction in which the
+! photons scatter may be different at the second and later scatterings. But
+! it is usually not that problematic.
+!
+! For Monte Carlo we are concerned with photons (or photon packages). We use
+! tau_scat = rho*kappa_scat*pathlength to find out IF a photon scatters.
+! 
+! ONCE we know that it scatters, the angular probability distribution
+! function for the direction of scattering is:
+!
+!               Z(mu,phi)[1,1] + Z(mu,phi)[1,2] * q + Z(mu,phi)[1,3] * u + Z(mu,phi)[1,4] * v
+!  P(mu,phi) = -------------------------------------------------------------------------------
+!                                            kappa_scat
+!
+! where the q, u and v are those of the incoming radiation.
+!
+! The P(mu,phi) is normalized in (mu,phi) space to unity:
+! 
+!   /+1     /2pi
+!   |   dmu |   dphi P(mu,phi) = 1
+!   /-1     /0
+!
+! so P(mu,phi) is the probability per dmu and per dphi. 
+!
+! So to find the new direction of scattering we use P(mu,phi) as the
+! random-number distribution function and pick a direction. Once we know the
+! direction, we still need to figure out what the q,u,v are. For that we
+! apply the Z(mu,phi) in the 3-stage way we discussed above.
+!
+! We can work out P(mu,phi) in terms of Z(mu) matrix elements by performing,
+! at a given value of phi, a basis rotation of (x',y') to (x'',y'') such
+! that the x''-axis points into the direction of scattering, i.e.
+! the e_x'' basis vector equals the e_x' vector rotated by phi in 
+! counter-clockwise direction. According to the above rules for
+! rotation (with ang=phi) the new q'', u'' and v'' then become:
+!
+!   q'' =  cos(2*phi)*q + sin(2*phi)*u
+!   u'' = -sin(2*phi)*q + cos(2*phi)*u
+!
+! Since in the (x'',y'') coordinates the scattering is now in the positive
+! x''-direction, we have that the probability for scattering is
+! P=(Z(mu)[1,1]+Z(mu)[1,2]*q'')/kappa_scat because the other matrix elements
+! are zero in this coordinate system.  Inserting the above expression for
+! q'' yields:
+!
+!               Z(mu)[1,1] + Z(mu)[1,2] * ( cos(2*phi)*q + sin(2*phi)*u )
+!  P(mu,phi) = -----------------------------------------------------------
+!                                    kappa_scat
+!
+! We can integrate this over phi to get the P(mu):
+!
+!                   Z(mu)[1,1]
+!   P(mu) =  2 pi  ------------
+!                   kappa_scat
+!
+! This shows that, for the probability of scattering with a certain
+! angle theta=acos(mu), the input polarization state is irrelevant.
+! In other words: the mu-phase function for scattering is entirely
+! given by Z(mu)[1,1]. Only the phi-phase function depends on the
+! input polarization state.
+!
+! We can also integrate P(mu,phi) over mu:
+!
+!
+!             Zint[1,1] + Zint[1,2] * ( cos(2*phi)*q + sin(2*phi)*u )
+!   P(phi) = ---------------------------------------------------------
+!                           2*pi * kappa_scat
+!
+! with 
+!
+!                    /+1
+!   Zint[1,i] = 2 pi |  dmu Z(mu)[1,i]
+!                    /-1
+!
+! Note that Zint[1,1] == kappa_scat.
+!
+! Now let's return to the question how to find the new direction of the
+! photon using P(mu,phi) as probability distribution function. We do this in
+! two stages.
+!
+!  Stage 1: Find phi using P(phi)
+!
+!  Stage 2: Compute P(mu|phi), which is the probability function for
+!           scattering at an angle mu, given angle phi. P(mu|phi) is:
+!
+!                                    /+1
+!            P(mu|phi) = P(mu,phi) / |  dmu' P(mu',phi)
+!                                    /-1 
+!
+!                        P(mu,phi)
+!                      = ---------
+!                         P(phi)
+!
+!            Find mu from P(mu|phi).
+!
+! Once we found both mu and phi, we can rotate (1,q,u,v) to the new phi, and
+! multiply with the scattering matrix to get (i'',q'',u'',v''). However, since
+! by definition i'' should become 1, because we have dealt with the
+! amplitude of the scattering by using kappa_scat and the P(mu,phi), we
+! obtain the new q,u,v as:
+!
+!   q_new = q''/i'',   u_new = u''/i''  and v_new = v''/i''
+!   
+! That should be it for the Monte Carlo scattering event. 
+!
+! NOTE: For randomly-oriented and mirror-symmetric particle ensembles
+!       the extinction matrix is scalar, i.e. the extinction coefficient
+!       is the same for I,Q,U,V and there is no mixing. In other words:
+!       K = diag(1,1,1,1)*kappa. This will NOT be the case for oriented
+!       particle ensembles.
+! 
+!------------------------------------------------------------------------
 """
 
 
